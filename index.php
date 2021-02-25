86d91adf59cd7cddfdbbc4146c3c990846c5c6a9
userlg <guipeluis90@gmail.com>
2021-02-25 13:55:47 -0400

    Comienzo de la aplicacion para Clinica Dental

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="style.css">
+     <title>Dentist App</title>
+</head>
+<body>
+
+<?php
+
+  echo '<h2> Hola desde Php </h2>';
+
+ ?>
+     
+</body>
+</html>
