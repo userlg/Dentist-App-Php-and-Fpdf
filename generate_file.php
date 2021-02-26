b8548e17b82ce26f1ef18d24e6741b3b4de01878
userlg <guipeluis90@gmail.com>
2021-02-25 23:41:58 -0400

    Corregido el error de Fpdf, ya se genera el arhivo, queda pendiente crear el diseño y el formato

---
+++ generate_file.php
@@ -0,0 +1,19 @@
+<?php
+
+require ('FPDF/fpdf.php');
+
+$Now = date("Ymdsh");  
+
+//echo $Now; esta variable almacena la fecha en un formato creado
+
+$pdf = new FPDF();
+
+$pdf->AddPage();
+
+$pdf->SetFont('Arial','B',16);
+
+$pdf->Cell(40,10,'Hola, Fpdf desde php');
+
+$pdf->Output('I',$Now.'.pdf');
+
+?>
--- index.php
+++ index.php
@@ -5,30 +5,27 @@
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link rel="stylesheet" href="style.css">
-     <title>Dentist App</title>
+     <title>Document</title>
 </head>
 <body>
 
-<?php
+ <h1>Generador de Pdf Files</h1>
 
-  require ('FPDF/fpdf.php');
 
-  echo '<h2> Hola desde Php </h2>';
+ <?php
 
-$pdf=new FPDF();
-//Primera página
-$pdf->AddPage();
 
-$pdf->SetFont('Arial','B',16);
 
-$pdf->Cell(40,10,'¡Mi primera página pdf con FPDF!');
+//var_dump($xdate);
 
-#$pdf->Image('leon.jpg' , 80 ,22, 35 , 38,'JPG', 'http://www.desarrolloweb.com');
-$pdf->Close();
 
-$pdf->Output();
+
+
+
+//echo date("d-m-YH:i:s", $time);
 
  ?>
+
      
 </body>
 </html>
