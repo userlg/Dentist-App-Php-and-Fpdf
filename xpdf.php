ee01e2ce6a6c007ca9e385823b76ed13e7c14df5
userlg <guipeluis90@gmail.com>
2021-03-06 08:25:32 -0400

    Se ha logrado insertar correctamente la imagen en el reporte, ahora queda acomodar las demas imagenes y estilizar mas el diseño del reporte

--- xpdf.php
+++ xpdf.php
@@ -29,7 +29,7 @@ function description_file_upload($file_name)
 
 require('FPDF/fpdf.php');
 
-description_file_upload('image');
+//description_file_upload('image');
 
 $file_name = 'image';
 
@@ -43,6 +43,8 @@ $dat = $_FILES[$file_name]['tmp_name'];
 
 $size = $_FILES[$file_name]['size'];
 
+// El directorio images es uno temporal que se ha creado para procesar las imagenes mientras se genera el reporte
+
 
 $path = 'images/';
 
@@ -50,14 +52,14 @@ $path = $path . basename($_FILES['image']['name']);
 
 
 
-if (move_uploaded_file($dat, $path)) {
+/*if (move_uploaded_file($dat, $path)) {
      echo 'Archivo Movido de forma exitosa';
 } else
      echo 'Ha ocurrido un error';
+*/
 
-unlink($path);
 
-/*$doc = new FPDF('P','mm','Letter');
+$doc = new FPDF('P','mm','Letter');
 
 $doc->AddPage();
 
@@ -71,16 +73,21 @@ $doc->Cell(40,60,'Prueba',1,1,'C',true);
 
 $doc->Ln(5);
 
-//$doc->Image('images/image.png',10,15,50,60,'PNG');
+move_uploaded_file($dat, $path);
+
+//$doc->Cell(95,80, $doc->Image($path, $doc->GetX()+2, $doc->GetY()+2, 90) ,1,"C");
+
+$doc->Image($path, 40,50,60,40,'PNG');
+
+unlink($path); // Se borra la imagen despues de insertarla en el reporte
 
 $doc->Ln(5);
 
 $doc->Cell(40,60,$x_file,0,1,'C',true);
 
-//unlink('img.jpg');
-
-
 $doc->Ln(5);
 
-$doc->Output('I',  'f235.pdf',true);
-*/
+$doc->Output('I', 'f235.pdf',true);
+
+unlink($path);
+
